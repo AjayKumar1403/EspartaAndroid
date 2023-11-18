5455a4b13ef140d15ddfd15e8d04cf6df3ddff78
Ajay Kumar Dhonthoju <123030130+AjayKumar1403@users.noreply.github.com>
2023-11-17 23:35:52 -0600

    Update HomeActivity.java
    Modified the file

--- app/src/main/java/com/example/esparta_android/HomeActivity.java
+++ app/src/main/java/com/example/esparta_android/HomeActivity.java
@@ -1,6 +1,12 @@
 package com.example.esparta_android;
 
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,57 +14,47 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-import com.example.espartaandro.R;
+
+import com.example.espartaandro.Adapters.ImageAdapter;
+import com.example.espartaandro.Model.ImageData;
 
 import io.paperdb.Paper;
 
+
+
 public class HomeActivity extends AppCompatActivity {
 
-
-    private Button logout_btn;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-//        logout_btn = findViewById(R.id.logout_btn);
 
+        // Create an array of ImageData objects
+        ImageData[] imageDatas = {
+                new ImageData(R.drawable.art1, "Title 1", "Description 1"),
+                new ImageData(R.drawable.art2, "Title 2", "Description 2"),
+                // Add more objects as needed
+        };
 
-//        indianHistory.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(HomeActivity.this, AdminAddNewProductActivity.class);
-//                intent.putExtra("category","indianHistory");
-//                startActivity(intent);
-//            }
-//        });
-//
-//
-//        landScapeArt.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(HomeActivity.this, AdminAddNewProductActivity.class);
-//                intent.putExtra("category", "landScapeArt");
-//                startActivity(intent);
-//            }
-//        });
-//
-//        stillArt.setOnClickListener(new View.OnClickListener() {
-//            @Overridew
-//            public void onClick(View view) {
-//                Intent intent = new Intent(HomeActivity.this, AdminAddNewProductActivity.class);
-//                intent.putExtra("category", "stillArt");
-//                startActivity(intent);
-//            }
-//        });
+        recyclerView = findViewById(R.id.recyclerView);
+        ImageAdapter imageAdapter = new ImageAdapter(imageDatas);
 
-//        logout_btn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Paper.book().destroy();
-//                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
-//                startActivity(intent);
-//            }
-//        });7
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        recyclerView.setAdapter(imageAdapter);
+
+        imageAdapter.setOnItemClickListener(new ImageAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                ImageData selectedImageData = imageDatas[position];
+
+                Intent intent = new Intent(HomeActivity.this, ItemView.class);
+                intent.putExtra("imageResource", selectedImageData.getImageResource());
+                intent.putExtra("title", selectedImageData.getTitle());
+                intent.putExtra("description", selectedImageData.getDescription());
+                startActivity(intent);
+            }
+        });
     }
 }
